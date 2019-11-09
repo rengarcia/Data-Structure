932bed61b5ec362b32fce6762b7de02a74b91e2e
Kevinhurtado <44406663+Kevinhurtado@users.noreply.github.com>
2019-11-09 14:23:59 -0500

    Trabajo

---
+++ Sudoku/Codigo/Aplicativo.cpp
@@ -0,0 +1,18 @@
+/**
+  * Universidad De las Fuerzas Armadas ESPE
+  * Carrera: Ingeniería de Software
+  * Materia: Estructura de Datos
+  * Tema: Sudoku
+  * NRC: 2969
+*/
+
+#include "Sudoku.cpp"
+
+int main() {
+	Sudoku sudoku;
+	int **matriz;
+	matriz = sudoku.hacerSolucion(matriz);
+	sudoku.tablero(matriz);
+	return 0;
+}
+
