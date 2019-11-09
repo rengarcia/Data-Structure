1e944b9615f806ad2b2e2c34e3ca502519f2e13f
Andres <andresgarcia17@icloud.com>
2019-11-09 08:25:14 -0500

    first commit

---
+++ Metodos de ordenamiento/Quicksort/Codigo/Ingreso.h
@@ -0,0 +1,124 @@
+/*Universidad de las fuerzas armadas ESPE
+ * Estructura de Datos
+ * Ingenieria de Software
+ * Renan Garcia
+ * Pregrado S-II
+ * creacion: 14 de Octubre
+ * modificacion: 18 de octubre
+ */
+
+#ifndef INGRESO_H
+#define INGRESO_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Ingreso {
+public:
+    string ingresar(string);
+    bool validarTipoFloat(string);
+    bool validarTipoInt(string);
+    bool validarTipoString(string);
+};
+/**
+ *
+ * @brief Ingresa datos desde teclado a valor
+ * @param msg el mensage de ingreso
+ * @return la conversion de string a flotante
+ */
+string Ingreso::ingresar(string msg) {
+    string dato_a_validar;
+    bool esValido = false;
+
+    while (!esValido) {
+        try {
+            cout << msg;
+            getline(cin, dato_a_validar);
+            if (msg.find("flotante") != std::string::npos) {
+                esValido = validarTipoFloat(dato_a_validar);
+            }
+            else if (msg.find("entero") != std::string::npos) {
+                esValido = validarTipoInt(dato_a_validar);
+            }
+            else if (msg.find("cadena") != std::string::npos) {
+                esValido = validarTipoString(dato_a_validar);
+            }
+
+            if (!esValido) {
+                throw dato_a_validar;
+            }
+        }
+        catch (string e) {
+            cout << "El dato (" << e << ") no es valido" << endl;
+        }
+    }
+    return dato_a_validar;
+}
+/**
+ *
+ * @brief Valida si el numero ingresado es realmente un float
+ * @param numero el numero ingresado por usuario
+ * @return un booleano verdadero si el numero es float y falso si no lo es
+ */
+bool Ingreso::validarTipoFloat(string numero) {
+    int inicio = 0;
+    if (numero.length() == 0) {
+        return 0;
+    }
+    if (numero[0] == '+' || numero[0] == '-') {
+        inicio = 1;
+        if (numero.length() == 1) {
+            return 0;
+        }
+    }
+    for (int i = inicio; i < numero.length(); i++) {
+        if (!isdigit(numero[i]) && numero[i] != '.') {
+            return 0;
+        }
+    }
+    return 1;
+}
+/**
+ *
+ * @param numero string a verificar si es Int
+ * @return un booleano verdadero si es entero, caso contrario falso
+ */
+bool Ingreso::validarTipoInt(string numero) {
+    int inicio = 0;
+    if (numero.length() == 0) {
+        return 0;
+    }
+    if (numero[0] == '+' || numero[0] == '-') {
+        inicio = 1;
+        if (numero.length() == 1) {
+            return 0;
+        }
+    }
+    for (int i = inicio; i < numero.length(); i++) {
+        if (!isdigit(numero[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+/**
+ *
+ * @param numero cadena a validar si es un string
+ * @return un booleano verdadero si es string, caso contrario falso
+ */
+bool Ingreso::validarTipoString(string numero) {
+    if (numero.length() == 0) {
+        return 0;
+    }
+
+    for (int i = 0; i < numero.length(); i++) {
+        if (isdigit(numero[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+#endif //INGRESO_H
---
+++ Metodos de ordenamiento/Quicksort/Codigo/main.cpp
@@ -0,0 +1,26 @@
+/*Universidad de las fuerzas armadas ESPE
+ * Estructura de Datos
+ * Ingenieria de Software
+ * Renan Garcia
+ * Pregrado S-II
+ * creacion: 14 de Octubre
+ * modificacion: 18 de octubre
+ */
+#include <iostream>
+#include "Quicksort.h"
+#include "Ingreso.h"
+
+constexpr auto largo = 20;
+using namespace std;
+
+int main()
+{
+    Ingreso ingreso;
+    int n = atoi(ingreso.ingresar("Indique valor entero de numeros a ingresar: ").c_str());
+    int* A = (int*)malloc(n * sizeof(int));
+
+    leeCadena(n, A);
+    quicksort(A, 0, n - 1);
+    muestraCadena(n, A);
+    return 0;
+}
