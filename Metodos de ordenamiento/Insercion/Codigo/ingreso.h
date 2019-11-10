96940fc1a561af0d82c5977d37080d93361f176e
Jonathan <44239146+japicado@users.noreply.github.com>
2019-11-10 11:46:45 -0500

    Modelado Incersion

--- Metodos de ordenamiento/Insercion/Codigo/ingreso.h
+++ Metodos de ordenamiento/Insercion/Codigo/ingreso.h
@@ -16,131 +16,131 @@ NRC: 2967
 using namespace std;
 
 class Ingreso {
-	public:
-		int ingresarEntero();
-		double ingresarDouble();
-		float ingresarFloat();
-		string ingresarLetra();
-		bool validar(string);
-		bool validarEntero(string);
-		bool validarLetra(string);
+public:
+    int ingresarEntero();
+    double ingresarDouble();
+    float ingresarFloat();
+    string ingresarLetra();
+    bool validar(string);
+    bool validarEntero(string);
+    bool validarLetra(string);
 };
 
 int Ingreso::ingresarEntero() {
-		string numero;
-		bool valido = false;
-		while(!valido) {
-			try {
-				getline(cin,numero);
-				valido = validarEntero(numero);
-				if(!valido) {
-					throw numero;
-				}
-			}catch(string e) {
-				cout << "El numero " << e << " no es valido" << endl;
-			}
-		}
-		return atoi(numero.c_str());
-	}
-	
+    string numero;
+    bool valido = false;
+    while (!valido) {
+        try {
+            getline(cin, numero);
+            valido = validarEntero(numero);
+            if (!valido) {
+                throw numero;
+            }
+        } catch (string e) {
+            cout << "El numero " << e << " no es valido" << endl;
+        }
+    }
+    return atoi(numero.c_str());
+}
+
 double Ingreso::ingresarDouble() {
-		string numero;
-		bool valido = false;
-		while(!valido) {
-			try {
-				getline(cin,numero);
-				valido = validar(numero);
-				if(!valido) {
-					throw numero;
-				}
-			}catch(string e) {
-				cout << "El numero " << e << " no es valido" << endl;
-			}
-		}
-		return atof(numero.c_str());
-	}
-	
+    string numero;
+    bool valido = false;
+    while (!valido) {
+        try {
+            getline(cin, numero);
+            valido = validar(numero);
+            if (!valido) {
+                throw numero;
+            }
+        } catch (string e) {
+            cout << "El numero " << e << " no es valido" << endl;
+        }
+    }
+    return atof(numero.c_str());
+}
+
 float Ingreso::ingresarFloat() {
-		string numero;
-		bool valido = false;
-		while(!valido) {
-			try {
-				getline(cin,numero);
-				valido = validar(numero);
-				if(!valido) {
-					throw numero;
-				}
-			}catch(string e) {
-				cout << "El numero " << e << " no es valido" << endl;
-			}
-		}
-		return atof(numero.c_str());
-	}
-	
+    string numero;
+    bool valido = false;
+    while (!valido) {
+        try {
+            getline(cin, numero);
+            valido = validar(numero);
+            if (!valido) {
+                throw numero;
+            }
+        } catch (string e) {
+            cout << "El numero " << e << " no es valido" << endl;
+        }
+    }
+    return atof(numero.c_str());
+}
+
 string Ingreso::ingresarLetra() {
-		string palabra;
-		bool valido = false;
-		while(!valido) {
-			try {
-				getline(cin,palabra);
-				valido = validarLetra(palabra);
-				if(!valido) {
-					throw palabra;
-				}
-			}catch(string e) {
-				cout << "La palabra " << e << " no es valida" << endl;
-			}
-		}
-		return palabra;
-	}
-	
+    string palabra;
+    bool valido = false;
+    while (!valido) {
+        try {
+            getline(cin, palabra);
+            valido = validarLetra(palabra);
+            if (!valido) {
+                throw palabra;
+            }
+        } catch (string e) {
+            cout << "La palabra " << e << " no es valida" << endl;
+        }
+    }
+    return palabra;
+}
+
 bool Ingreso::validar(string numero) {
-		int inicio = 0;
-		if(numero.length() == 0) {
-			return 0;
-		}
-		if(numero[0] == '+' || numero[0] == '-') {
-			inicio = 1;
-			if(numero.length() == 1) {
-				return 0;
-			}
-		}
-		for(int i=inicio; i<numero.length(); i++) {
-			if(!isdigit(numero[i]) && numero[i] != '.') {
-				return 0;
-			}
-		}
-		return 1;
-	}
-	
+    int inicio = 0;
+    if (numero.length() == 0) {
+        return 0;
+    }
+    if (numero[0] == '+' || numero[0] == '-') {
+        inicio = 1;
+        if (numero.length() == 1) {
+            return 0;
+        }
+    }
+    for (int i = inicio; i < numero.length(); i++) {
+        if (!isdigit(numero[i]) && numero[i] != '.') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 bool Ingreso::validarEntero(string numero) {
-		int inicio = 0;
-		if(numero.length() == 0) {
-			return 0;
-		}
-		if(numero[0] == '+' || numero[0] == '-') {
-			inicio = 1;
-			if(numero.length() == 1) {
-				return 0;
-			}
-		}
-		for(int i=inicio; i<numero.length(); i++) {
-			if(!isdigit(numero[i])) {
-				return 0;
-			}
-		}
-		return 1;
+    int inicio = 0;
+    if (numero.length() == 0) {
+        return 0;
+    }
+    if (numero[0] == '+' || numero[0] == '-') {
+        inicio = 1;
+        if (numero.length() == 1) {
+            return 0;
+        }
+    }
+    for (int i = inicio; i < numero.length(); i++) {
+        if (!isdigit(numero[i])) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 bool Ingreso::validarLetra(string palabra) {
-	char c;
-	for(int i=0; i<palabra.size(); i++) {
-		c=palabra[i];
-		if(isalpha(c) == 0) {
-			if(isspace(c) == 0) {
-				return 0;
-			}
-		}
-	}
-	return 1;
+    char c;
+    for (int i = 0; i < palabra.size(); i++) {
+        c = palabra[i];
+        if (isalpha(c) == 0) {
+            if (isspace(c) == 0) {
+                return 0;
+            }
+        }
+    }
+    return 1;
 }
--- Metodos de ordenamiento/Insercion/Codigo/insercion.cpp
+++ Metodos de ordenamiento/Insercion/Codigo/insercion.cpp
@@ -14,15 +14,15 @@ NRC: 2967
 #include "ingreso.h"
 
 int main() {
-	Ingreso leer;
-	Insercion insercion;
-	int *vector,tamanio;
-	printf("Ingrese el tamanio del vector: ");
-	tamanio = leer.ingresarEntero();
-	printf("Ingrese los datos del vector: ");
-	vector = insercion.ingresaNumeros(tamanio,vector);
-	insercion.mostrar(tamanio,vector);
-	printf("\nEl vector ordenado es: \n");
-	insercion.insercionRecursivo(tamanio,vector);
-	insercion.mostrar(tamanio,vector);
+    Ingreso leer;
+    Insercion insercion;
+    int *vector, tamanio;
+    printf("Ingrese el tamanio del vector: ");
+    tamanio = leer.ingresarEntero();
+    printf("Ingrese los datos del vector: ");
+    vector = insercion.ingresaNumeros(tamanio, vector);
+    insercion.mostrar(tamanio, vector);
+    printf("\nEl vector ordenado es: \n");
+    insercion.insercionRecursivo(tamanio, vector);
+    insercion.mostrar(tamanio, vector);
 }
