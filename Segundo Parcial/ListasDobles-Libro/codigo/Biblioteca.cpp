17faedcb369af05182fefc4628b621d97ef7c0b6
luisE1996 <44406616+luisE1996@users.noreply.github.com>
2019-11-28 10:40:43 -0500

    buscar por id

--- Segundo Parcial/ListasDobles-Libro/codigo/Biblioteca.cpp
+++ Segundo Parcial/ListasDobles-Libro/codigo/Biblioteca.cpp
@@ -53,9 +53,27 @@ void Biblioteca::borrarLibro(int idLibro)
 // Return:     void
 ////////////////////////////////////////////////////////////////////////
 
-void Biblioteca::buscarLibro(int idLibro)
+int Biblioteca::buscarLibro(Nodo *&nodo, int idLibro)
 {
-   // TODO : implement
+    Nodo *aux = new Nodo();
+    aux = nodo;
+    int posicion=1, cont=0;
+    while(aux != NULL)
+    {
+        if(aux->getlibro().getidLibro() == idLibro)
+        {
+            cout<<"Nombre del libro: "<<aux->getlibro().gettitulo()<<endl;
+            cout<<"Encontrado en la posicion: "<<posicion<<endl;
+            cont++;
+        }
+        posicion++;
+        aux = aux->getSiguiente();
+    }
+    if(cont==0){
+        cout<<"Nombre no encontrado"<<endl;
+        return 0;
+    }
+        return posicion;
 }
 
 ////////////////////////////////////////////////////////////////////////
